e51b756ca25799e7c889695b42429182fa33458f
iwumezie Pamela <iwumeziep@gmail.com>
2023-04-14 20:54:59 +0100

    added animation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import image from '../assets/pamela.jpg'
-import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
+import React from 'react';
+import image from '../assets/pamela.jpg';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { Slide } from 'react-awesome-reveal';
 
 const About = () => {
   return (
@@ -16,37 +17,40 @@ const About = () => {
       </h1>
 
       <div className='flex flex-col justify-center max-w-screen-lg mx-auto h-full w-full md:flex-row '>
-        <div className=' mb-6 '>
-          <img
-            src={image}
-            alt='my profile'
-            className=' md:max-w-5xl rounded-2xl mx-auto w-[50%] md:w-[50%] lg:w-[45%] '
-          />
-        </div>
-
-        <div>
-          <p className='max-w-md p-2 text-center md:text-left leading-7 pt-8 md:pt-0 md:max-w-[1042px] mx-auto lg:text-lg'>
-            I am a Technically astute front-end developer with an experience in
-            managing and configuring ReactJs. A proven track record of hands on
-            experience in Html,Css and JavaScript in analysing,designing and
-            implementing solutions to drive positive changes to optimize
-            business processes. Seeking a challenging role where i can deploy my
-            IT skill set to make transformative differences whilst nurturing a
-            thriving developer career pathway
-          </p>
-          <a
-            href='#projects'
-            className='md:mx-0 md:my-4 group w-fit flex items-center rounded-md border-2 border-emerald-400 py-3 px-10 my-8 capitalize hover:bg-gray-100 text-lg hover:border-white transition-all ease-in-out duration-300  hover:text-[#000300] tracking-widest cursor-pointer mx-auto'
-          >
-            portfolio
-            <span className='group-hover:rotate-90 duration-300'>
-              <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-            </span>
-          </a>
-        </div>
+        <Slide direction='left'>
+          <div className=' mb-6 '>
+            <img
+              src={image}
+              alt='my profile'
+              className=' md:max-w-5xl rounded-2xl mx-auto w-[50%] md:w-[50%] lg:w-[45%] '
+            />
+          </div>
+        </Slide>
+        <Slide direction='right'>
+          <div>
+            <p className='max-w-md p-2 text-center md:text-left leading-7 pt-8 md:pt-0 md:max-w-[1042px] mx-auto lg:text-lg'>
+              I am a Technically astute front-end developer with an experience
+              in managing and configuring ReactJs. A proven track record of
+              hands on experience in Html,Css and JavaScript in
+              analysing,designing and implementing solutions to drive positive
+              changes to optimize business processes. Seeking a challenging role
+              where i can deploy my IT skill set to make transformative
+              differences whilst nurturing a thriving developer career pathway
+            </p>
+            <a
+              href='#projects'
+              className='md:mx-0 md:my-4 group w-fit flex items-center rounded-md border-2 border-emerald-400 py-3 px-10 my-8 capitalize hover:bg-gray-100 text-lg hover:border-white transition-all ease-in-out duration-300  hover:text-[#000300] tracking-widest cursor-pointer mx-auto'
+            >
+              portfolio
+              <span className='group-hover:rotate-90 duration-300'>
+                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+              </span>
+            </a>
+          </div>
+        </Slide>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TypeAnimation } from 'react-type-animation';
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+import { Slide } from 'react-awesome-reveal';
 const Home = () => {
   return (
     <section
@@ -8,56 +9,58 @@ const Home = () => {
       className='bg-gradient-to-b from-black to-gray-900 md:px-20 text-white h-screen w-full'
     >
       <div className='max-w-screen-md flex flex-col h-full w-full items-center justify-center  md:items-start'>
-        <h1 className='text-xl uppercase text-center md:text-left tracking-wide font-bold pb-4'>
-          👋 Hello there, i'm
-          <span className='text-emerald-400 font-extrabold'> Pamela</span>
-        </h1>
-        <h2 className=' font-extrabold text-center uppercase text-2xl pb-4 md:text-5xl lg:text-6xl '>
-          A frontend developer
-        </h2>
-        <p className='text-base md:text-lg text-center md:text-left max-w-sm  md:max-w-2xl text-gray-200 pb-2 md:mx-0 px-2 mx-auto '>
-          I'm a creative developer devoted to crafting user friendly and
-          interactive frontend websites focused on simplicity and purpose.
-        </p>
-        <div className='flex items-center px-2 pb-3'>
-          <p className='text-base lg:text-lg mr-1'>I'm proficient in </p>
-          <TypeAnimation
-            sequence={[
-              '< Html />',
-              2000,
-              '< Css />',
-              2000,
-              '< Js />',
-              2000,
-              '< Tailwind Css />',
-              2000,
-              '< React js />',
-              2000,
-              '< Git & Github />',
-              2000,
-              '< Firebase />',
-              2000,
-              '< Styled Component />',
-              2000,
-            ]}
-            wrapper='span'
-            cursor={true}
-            repeat={Infinity}
-            className='text-emerald-400 inline-block text-lg lg:text-xl font-bold'
-          />
-        </div>
+        <Slide direction='left'>
+          <h1 className='text-xl uppercase text-center md:text-left tracking-wide font-bold pb-4'>
+            👋 Hello there, i'm
+            <span className='text-emerald-400 font-extrabold'> Pamela</span>
+          </h1>
+          <h2 className=' font-extrabold text-center uppercase text-2xl pb-4 md:text-5xl lg:text-6xl '>
+            A frontend developer
+          </h2>
+          <p className='text-base md:text-lg text-center md:text-left max-w-sm  md:max-w-2xl text-gray-200 pb-2 md:mx-0 px-2 mx-auto '>
+            I'm a creative developer devoted to crafting user friendly and
+            interactive frontend websites focused on simplicity and purpose.
+          </p>
+          <div className='flex items-center px-2 pb-3'>
+            <p className='text-base lg:text-lg mr-1'>I'm proficient in </p>
+            <TypeAnimation
+              sequence={[
+                '< Html />',
+                2000,
+                '< Css />',
+                2000,
+                '< Js />',
+                2000,
+                '< Tailwind Css />',
+                2000,
+                '< React js />',
+                2000,
+                '< Git & Github />',
+                2000,
+                '< Firebase />',
+                2000,
+                '< Styled Component />',
+                2000,
+              ]}
+              wrapper='span'
+              cursor={true}
+              repeat={Infinity}
+              className='text-emerald-400 inline-block text-lg lg:text-xl font-bold'
+            />
+          </div>
 
-        <div className=''>
-          <a
-            href='#contacts'
-            className='group w-fit flex items-center rounded-md border-2 border-emerald-400 py-3 px-4  my-2 capitalize hover:bg-gray-100 text-lg hover:border-white transition-all ease-in-out duration-300  hover:text-[#000300] tracking-widest cursor-pointer'
-          >
-            Lets connect
-            <span className='group-hover:rotate-90 duration-300'>
-              <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
-            </span>
-          </a>
-        </div>
+          <div className='px-2'>
+            <a
+              href='#contacts'
+              className='group w-fit flex items-center rounded-md border-2 border-emerald-400 py-3 px-4  my-2 capitalize hover:bg-gray-100 text-lg hover:border-white transition-all ease-in-out duration-300  hover:text-[#000300] tracking-widest cursor-pointer'
+            >
+              Lets connect
+              <span className='group-hover:rotate-90 duration-300'>
+                <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+              </span>
+            </a>
+          </div>
+        </Slide>
       </div>
     </section>
   );
