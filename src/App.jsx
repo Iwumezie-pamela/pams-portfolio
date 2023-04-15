25f36e05556b7d04d2e30d2f0ecb742ee0c943b4
iwumezie Pamela <iwumeziep@gmail.com>
2023-04-15 16:56:48 +0100

    added framer motion animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import About from './components/About'
-import Home from './components/Home'
-import Navbar from './components/Navbar'
-import Experience from './components/Experience'
-import Projects from './components/Project'
-import Contact from './components/Contact'
-import Socials from './components/Socials'
-
+import { useState } from 'react';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Experience from './components/Experience';
+import Projects from './components/Project';
+import Contact from './components/Contact';
+import Socials from './components/Socials';
 function App() {
   return (
-    <div className='App'>
+    <>
       <Navbar />
       <Home />
       <About />
@@ -17,8 +16,8 @@ function App() {
       <Projects />
       <Contact />
       <Socials />
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,7 +12,7 @@ import git from '../assets/git.png';
 import github from '../assets/github.png';
 
 const Experience = () => {
-  const { ref, inView } = useInView();
+  // const { ref, inView } = useInView();
   const techStacks = [
     {
       id: 1,
@@ -66,74 +66,72 @@ const Experience = () => {
 
   return (
     <section
-      ref={ref}
+      // ref={ref}
       id='experience'
       className='bg-gradient-to-b from-black to-gray-900 w-full md:h-screen text-white p-4 '
     >
       <div className='max-w-screen-lg  p-4 mx-auto  flex flex-col justify-center w-full h-full'>
-        {inView && (
-          <motion.div
-            initial={{
-              x: -500,
-              opacity: 0,
-              scale: 0.5,
-            }}
-            animate={{
-              x: 0,
-              opacity: 1,
-              scale: 1,
-            }}
-            transition={{
-              duration: 1.5,
-            }}
-          >
-            <p className='text-4xl font-bold relative before:h-1 before:rounded-lg before:absolute before:bottom-0 before:w-28 md:pt-8  before:bg-emerald-400 pb-2 pt-16  '>
-              Tech Stacks
-            </p>
-            <p className='py-6 text-lg'>
-              These are the technologies i work with
-            </p>
-          </motion.div>
-        )}
+        {/* {inView && ( */}
+        <motion.div
+          initial={{
+            x: -500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+        >
+          <p className='text-4xl font-bold relative before:h-1 before:rounded-lg before:absolute before:bottom-0 before:w-28 md:pt-8  before:bg-emerald-400 pb-2 pt-16  '>
+            Tech Stacks
+          </p>
+          <p className='py-6 text-lg'>These are the technologies i work with</p>
+        </motion.div>
+        {/* )} */}
 
-        {inView && (
-          <motion.div
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            exit={{ scale: 0 }}
-            style={{
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-            }}
-            transition={{
-              duration: 1.5,
-            }}
-            className='w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'
-          >
-            {techStacks.map(({ id, image, name, style }) => {
-              return (
-                <div
-                  key={id}
-                  className={
-                    'group shadow-md hover:scale-105 duration-500 py-2 rounded-lg' +
-                    ' ' +
-                    style
-                  }
-                >
-                  <img
-                    src={image}
-                    alt='html'
-                    className='group-hover:scale-105 ease-in-out transition-all duration-500 w-20 mx-auto'
-                  />
-                  <p className='mt-4 text-[.6rem] md:text-[1rem]  uppercase'>
-                    {name}
-                  </p>
-                </div>
-              );
-            })}
-          </motion.div>
-        )}
+        {/* {inView && ( */}
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          exit={{ scale: 0 }}
+          style={{
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className='w-full grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'
+        >
+          {techStacks.map(({ id, image, name, style }) => {
+            return (
+              <div
+                key={id}
+                className={
+                  'group shadow-md hover:scale-105 duration-500 py-2 rounded-lg' +
+                  ' ' +
+                  style
+                }
+              >
+                <img
+                  src={image}
+                  alt='html'
+                  className='group-hover:scale-105 ease-in-out transition-all duration-500 w-20 mx-auto'
+                />
+                <p className='mt-4 text-[.6rem] md:text-[1rem]  uppercase'>
+                  {name}
+                </p>
+              </div>
+            );
+          })}
+        </motion.div>
+        {/* )} */}
       </div>
     </section>
   );
